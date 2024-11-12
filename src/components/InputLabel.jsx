c0449112971b5de14328b79d13c2947fa60bb66e
ayeshaArif6 <[aarif6@asu.edu]>
2024-11-11 22:25:34 -0500

    Added inputlabel class

---
+++ src/components/InputLabel.jsx
@@ -0,0 +1,22 @@
+import classNames from "classnames";
+import React from 'react';
+
+export default function InputLabel({
+                                       label,
+                                       layout = "horizontal",
+                                       input,
+                                   }) {
+    const containerClass = classNames("relative", {
+        "flex flex-col": layout === "vertical",
+        "flex justify-center": layout === "horizontal",
+    });
+
+    const labelClass = classNames("text-sm w-full text-left p-2");
+
+    return (
+        <div className={containerClass}>
+            {label && <label className={labelClass}>{`${label}:`}</label>}
+            {input}
+        </div>
+    );
+}
