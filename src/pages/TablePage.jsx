90a5f2ee24ad5e8e43dbf288064b61975303481c
evanhagood <evanmhagood@gmail.com>
2024-11-25 11:46:15 -0500

    Task 339: Add data flow for project name to ProjectSettings

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../components/Button';
 import TabBar from '../components/TabBar';
 import PageWrapper from '../wrappers/PageWrapper';
 
+/**
+ * Quick and dirty way to get the Current Project to TopNav for ProjectSettings.jsx
+ * @type {string}
+ */
+let currentProject = "null";
+export function getCurrentProject() {
+    return currentProject;
+}
+
 export default function TablePage({ Email }) {
     const [selectedTab, setSelectedTab] = useState('');
     const [selectedProject, setSelectedProject] = useState('');
+    useEffect(() => {
+        currentProject = selectedProject;
+    }, [selectedProject]);
 
     return (
         <PageWrapper>
