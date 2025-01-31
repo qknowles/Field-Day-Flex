8c67f8ecae97e9661e7b09655951581e8d2bf526
Timothy Weaver <tlweave2@asu.edu>
2025-01-31 07:29:07 +0000

    updated hamburger menu to use jotai and removedtable import

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,25 +1,23 @@
-import { LizardIcon } from '../assets/icons';
 import React, { useState } from 'react';
-import Button from './Button';
-import AccountSettings from '../windows/AccountSettings.jsx';
-import ProjectSettings from '../windows/ProjectSettings.jsx';
-import { getCurrentProject } from '../pages/TablePage.jsx';
-import { getDocumentIdByProjectName } from '../utils/firestore.js';
+import { useAtom } from 'jotai';
+import { currentProjectName } from '../utils/jotai';
+import AccountSettings from '../windows/AccountSettings';
+import ProjectSettings from '../windows/ProjectSettings';
 import ManageMembership from '../windows/MembershipWindow';
+import Button from './Button';  // Import Button component
 
 export default function Hamburger({ Email }) {
-    const [menuOpen, setMenuOpen] = useState(false); // Hamburger menu state
-    const [modalContent, setModalContent] = useState(null); // Modal content state
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [modalContent, setModalContent] = useState(null);
+    const [currentProject] = useAtom(currentProjectName);
     const [setCurrentWindow] = useState('HomePage');
 
     const toggleMenu = () => setMenuOpen((prev) => !prev);
 
-    // Function to handle button clicks and display modal with custom content
     const handleButtonClick = (content) => {
-        setModalContent(content); // Set the content to be displayed in the modal
+        setModalContent(content);
     };
 
-    // Function to close modal
     const closeModal = () => {
         setModalContent(null);
         setMenuOpen(false);
@@ -39,9 +37,9 @@ export default function Hamburger({ Email }) {
             {menuOpen && (
                 <div className="hamburger-menu text-black absolute mt-2 rounded-md shadow-lg p-2 dark:bg-neutral-700">
                     <ul>
-                        <li>
-                            <button
-                                className="flex rounded-md p-1.5 text-white whitespace-nowrap bg-asu-maroon border-2 border-transparent items-center mb-2 w-full"
+                        <li className="mb-2">
+                            <Button
+                                text="Manage Account"
                                 onClick={() =>
                                     handleButtonClick(
                                         <AccountSettings
@@ -50,13 +48,11 @@ export default function Hamburger({ Email }) {
                                         />,
                                     )
                                 }
-                            >
-                                Manage Account
-                            </button>
+                            />
                         </li>
-                        <li>
-                            <button
-                                className="flex rounded-md p-1.5 text-white whitespace-nowrap bg-asu-maroon border-2 border-transparent items-center mb-2 w-full"
+                        <li className="mb-2">
+                            <Button
+                                text="Memberships"
                                 onClick={() =>
                                     handleButtonClick(
                                         <ManageMembership
@@ -66,25 +62,21 @@ export default function Hamburger({ Email }) {
                                         />,
                                     )
                                 }
-                            >
-                                Memberships
-                            </button>
+                            />
                         </li>
                         <li>
-                            <button
-                                className="flex rounded-md p-1.5 text-white whitespace-nowrap bg-asu-maroon border-2 border-transparent items-center w-full"
+                            <Button
+                                text="Manage Project"
                                 onClick={() =>
                                     handleButtonClick(
                                         <ProjectSettings
-                                            projectNameProp={getCurrentProject()}
+                                            projectNameProp={currentProject}
                                             CloseProjectSettings={closeModal}
                                             emailProp={Email}
                                         />,
                                     )
                                 }
-                            >
-                                Manage Project
-                            </button>
+                            />
                         </li>
                     </ul>
                 </div>
