8472355f850a1a7218cfe35594b560bd55dce2c0
Timothy Weaver <tlweave2@asu.edu>
2025-04-03 12:39:59 -0700

    added info icon

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,34 @@ import { Notifier } from './components/Notifier';
 import { useAtomValue } from 'jotai';
 import { isAuthenticated } from './utils/jotai.js';
 import './index.css';
+import { InfoIconProvider } from './components/InfoIconContext';
 
 function App() {
     const authenticated = useAtomValue(isAuthenticated);
-
+    
     return (
-        <div className="flex flex-col w-full min-h-screen text-neutral-800 dark:text-neutral-200 select-none">
-            <Notifier />
-            <TopNav />
-            <div className="flex flex-grow">
-                {authenticated ? (
-                    <TablePage />
-                ) : (
-                    <HomePage />
-                )}
+        <InfoIconProvider
+            initialSettings={{
+                iconColor: 'text-blue-500',
+                tooltipColor: 'bg-neutral-800',
+                textColor: 'text-white',
+                defaultPosition: 'top',
+                defaultSize: 16,
+                defaultWidth: 200
+            }}
+        >
+            <div className="flex flex-col w-full min-h-screen text-neutral-800 dark:text-neutral-200 select-none">
+                <Notifier />
+                <TopNav />
+                <div className="flex flex-grow">
+                    {authenticated ? (
+                        <TablePage />
+                    ) : (
+                        <HomePage />
+                    )}
+                </div>
             </div>
-        </div>
+        </InfoIconProvider>
     );
 }
 
