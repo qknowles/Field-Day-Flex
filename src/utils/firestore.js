444898857ba5d46bdede275529a7b5cfeb9244b8
Timothy Weaver <tlweave2@asu.edu>
2025-04-19 06:09:28 +0000

    remove unused variable declariton

--- src/utils/firestore.js
+++ src/utils/firestore.js
@@ -168,9 +168,7 @@ export const getDocumentIdByEmailAndProjectName = async (email, projectName) => 
 };
 
 export async function addMemberToProject(projectId, field, newMemberEmail) {
-    const isValid = ['contributors', 'admins', 'owners'].some(
-        (validField) => validField.toLowerCase() === field.toLowerCase(),
-    );
+ 
 
     if (!['contributors', 'admins', 'owners'].includes(field)) {
         console.error(`Invalid field: ${field}. Must be 'contributors', 'admins', or 'owners'.`);
